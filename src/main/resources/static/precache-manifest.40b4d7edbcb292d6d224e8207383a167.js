36a90270295aad020f8d57eae6d25f948f249814
John Dekis <John.Dekis@SSINA.BIZ>
2019-03-28 10:24:34 -0400

    add react proxy info and config

---
+++ src/main/resources/static/precache-manifest.40b4d7edbcb292d6d224e8207383a167.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "fa945d502bb32c0c830d",
+    "url": "/static/js/main.a7d2452b.chunk.js"
+  },
+  {
+    "revision": "ee1eb915657932316532",
+    "url": "/static/js/2.22cb7fd7.chunk.js"
+  },
+  {
+    "revision": "fa945d502bb32c0c830d",
+    "url": "/static/css/main.7e625f1b.chunk.css"
+  },
+  {
+    "revision": "ee1eb915657932316532",
+    "url": "/static/css/2.1edf2b84.chunk.css"
+  },
+  {
+    "revision": "1ed4cd8d8833eea97f1381f889ec245f",
+    "url": "/index.html"
+  }
+];
